2bcd1d118a2330d0cadf32c28e0c8656cd5789b9
xapizmat <mvlegioner@gmail.com>
2020-09-10 10:56:32 +0300

    Second day

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -18,20 +18,66 @@ var keys = {
 var setting = {
   start: false,
   score: 0,
-  speed: 3
+  speed: 3,
+  traffic: 3
 };
+
+function getQuantityElements(heightElement) {
+  return document.documentElement.clientHeight / heightElement + 1;
+}
 
 function startGame() {
   start.classList.add('hide');
+
+  for (var i = 0; i < getQuantityElements(100); i++) {
+    var line = document.createElement('div');
+    line.classList.add('line');
+    line.style.top = i * 100 + 'px';
+    line.y = i * 100;
+    gameArea.appendChild(line);
+  }
+
+  for (var _i = 0; _i < getQuantityElements(300 * setting.traffic); _i++) {
+    var enemy = document.createElement('div');
+    enemy.classList.add('enemy'); // enemy.y = car.offsetHeight * setting.traffic * (i + 1);
+
+    enemy.y = -300 * setting.traffic * (_i + 1);
+    enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 100)) + 'px';
+    enemy.style.top = enemy.y + 'px';
+    enemy.style.background = 'transparent url("../image/enemy.png") center / contain no-repeat';
+    gameArea.appendChild(enemy);
+  }
+
   setting.start = true;
   gameArea.appendChild(car);
+  setting.x = car.offsetLeft;
+  setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
 }
 
 function playGame() {
-  console.log("Play Game!");
+  moveRoad();
+  moveEnemy();
 
   if (setting.start) {
+    if (keys.ArrowLeft && setting.x > 0) {
+      setting.x -= setting.speed;
+    }
+
+    if (keys.ArrowRight && setting.x < gameArea.offsetWidth - car.offsetWidth) {
+      setting.x += setting.speed;
+    }
+
+    if (keys.ArrowUp && setting.y > 0) {
+      setting.y -= setting.speed;
+    }
+
+    if (keys.ArrowDown && setting.y < gameArea.offsetHeight - car.offsetHeight) {
+      setting.y += setting.speed;
+    }
+
+    car.style.left = setting.x + 'px';
+    car.style.top = setting.y + 'px';
     requestAnimationFrame(playGame);
   }
 }
@@ -44,4 +90,29 @@ function startRun(event) {
 function stopRun() {
   event.preventDefault();
   keys[event.key] = false;
+}
+
+function moveRoad() {
+  var lines = document.querySelectorAll('.line');
+  lines.forEach(function (line) {
+    line.y += setting.speed + 2;
+    line.style.top = line.y + 'px';
+
+    if (line.y > document.documentElement.clientHeight) {
+      line.y = -100;
+    }
+  });
+}
+
+function moveEnemy() {
+  var enemy = document.querySelectorAll('.enemy');
+  enemy.forEach(function (item) {
+    item.y += setting.speed;
+    item.style.top = item.y + 'px';
+
+    if (item.y >= document.documentElement.clientHeight) {
+      item.y = -200 * setting.traffic;
+      item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 100)) + 'px';
+    }
+  });
 }
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const score = document.querySelector('.score'),
     gameArea = document.querySelector('.gameArea'),
     car = document.createElement('div');
 
-    car.classList.add('car');
+car.classList.add('car');
 
 // console.dir(start);
 
@@ -21,19 +21,63 @@ const keys = {
 const setting = {
     start: false,
     score: 0,
-    speed: 3
+    speed: 3,
+    traffic: 3
 };
+
+function getQuantityElements(heightElement) {
+    return document.documentElement.clientHeight / heightElement + 1;
+}
 
 function startGame() {
     start.classList.add('hide');
+
+    for (let i = 0; i < getQuantityElements(100); i++) {
+        const line = document.createElement('div');
+        line.classList.add('line');
+        line.style.top = (i * 100) + 'px'; 
+        line.y = i * 100;
+        gameArea.appendChild(line);
+    }
+
+    for (let i = 0; i < getQuantityElements(300 * setting.traffic); i++) {
+        const enemy = document.createElement('div');
+        enemy.classList.add('enemy');
+        // enemy.y = car.offsetHeight * setting.traffic * (i + 1);
+        enemy.y = -300 * setting.traffic * (i + 1);
+        enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 100)) + 'px';
+        enemy.style.top = enemy.y + 'px';
+        enemy.style.background = 'transparent url("../image/enemy.png") center / contain no-repeat';
+        gameArea.appendChild(enemy);
+    }
+
     setting.start = true;
     gameArea.appendChild(car);
+    setting.x = car.offsetLeft;
+    setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
 }
 
 function playGame() {
-    console.log("Play Game!");
+    moveRoad();
+    moveEnemy();
     if (setting.start) {
+        if (keys.ArrowLeft && setting.x > 0) {
+            setting.x -= setting.speed;
+        }
+        if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
+            setting.x += setting.speed;
+        }
+        if (keys.ArrowUp && setting.y > 0) {
+            setting.y -= setting.speed;
+        }
+        if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
+            setting.y += setting.speed;
+        }
+        
+        car.style.left = setting.x + 'px';
+        car.style.top = setting.y + 'px';
+
         requestAnimationFrame(playGame);
     }
 
@@ -50,3 +94,32 @@ function stopRun() {
 
 }
 
+function moveRoad() {
+    let lines = document.querySelectorAll('.line');
+    lines.forEach(function(line) {
+        line.y += setting.speed + 2;
+        line.style.top = line.y + 'px';
+
+        if(line.y > document.documentElement.clientHeight) {
+            line.y = -100;
+        }
+
+    }); 
+}
+
+function moveEnemy() {
+    let enemy = document.querySelectorAll('.enemy');
+
+    enemy.forEach(function(item) {
+        item.y += setting.speed;
+        item.style.top = item.y + 'px';
+
+        if (item.y >= document.documentElement.clientHeight) {
+            item.y = -200 * setting.traffic;
+            item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 100)) + 'px';
+
+        }
+    });
+
+   
+}
